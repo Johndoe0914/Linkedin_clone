75409dff0bad0a349b055e34036b46af63c9908f
jonathan diaz <jonathandiaz0914@gmail.com>
2021-01-18 13:33:40 -0500

    working registering and login with firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import './App.css';
+import { selectUser } from './features/userSlice';
+import Login from './components/Login';
+
+
 
 const App = () => {
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
 
 <Header />
-
+{!user ? (<Login /> ) : (
 <div className='app__body'>
     <Sidebar />
 
 <Feed />
 
 {/* Widgets*/}
-</div>
+</div>)}
+
 
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import { auth } from '../firebase';
+import login from '../features/userSlice';
+import './Login.css';
+
+const Login = () => {
+
+    const [userDetails, setUserDetails] = useState({
+        email: '',
+        name: '',
+        password:'',
+        profilePic: ''
+
+    })
+
+    const dispatch = useDispatch()
+
+    const loginToApp = (e)=> {
+        e.preventDefault()
+        
+    }
+
+    const handleChange = name => event => {
+        setUserDetails({...userDetails, [name]: event.target.value})
+        
+    }
+
+    const register = () => {
+        const {email, name, password, profilePic} = userDetails
+        console.log(userDetails)
+        
+        // if( email === "" || password === "" || name === "" ) {
+        //     return alert('Please complete the fields')
+        // }
+
+        auth.createUserWithEmailAndPassword(email, password)
+        .then((userAuth) => {
+            userAuth.user.updateProfile({
+                displayName: name,
+                photoURL: profilePic
+            })
+            .then(() => {
+                dispatch(
+                    login({
+                    email: userAuth.user.email,
+                    uid: userAuth.user.uid,
+                    displayName: name,
+                    photoURL: profilePic
+                }))
+            })
+        }).catch(error => {alert(error.message)})
+    }
+
+
+    return (
+        
+        
+        <div className='login'>
+          <img src='https://www.tmf-group.com/-/media/images/logos/case-study-logos/linkedin.png' alt='logo'/>
+        <form>
+            <input value={userDetails.name} onChange={handleChange('name')} type='text' placeholder='Full name required if registering'/>
+            <input value={userDetails.profilePic} onChange={ handleChange('profilePic')} type='text' placeholder='Profile pic URL (optional)'/>
+            <input value={userDetails.email}  onChange={ handleChange('email')} type='text' placeholder='Email'/>
+            <input value={userDetails.password} onChange={ handleChange('password')}type='password' placeholder='Password' />
+                <button type='submit' onClick={loginToApp} >Sign In</button>
+        </form>
+        <p>Not a member?{" "}
+        <span className='login__register' onClick={register} >Register Now</span>
+        </p>
+       
+        </div>
+    )
+}
+
+export default Login
